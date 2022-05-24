db0c34202522e1e8daee659f4da2dbd87ad553eb
AntonMytnik <AntMytnik@gmail.com>
2022-05-24 16:20:59 +0200

    last

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 var loadFile = function(event) {
-    console.log('d');
     var output = document.getElementById('placeholder_img');
     output.src = URL.createObjectURL(event.target.files[0]);
     output.style.height = "210px";
